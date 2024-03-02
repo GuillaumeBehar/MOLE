73580e663887a80052209ddb9432101f084cbf4d
Znium <crancee.eliott@gmail.com>
2024-03-03 00:39:29 +0100

    update llm classes

--- utils/utils.py
+++ utils/utils.py
@@ -19,13 +19,13 @@ def save_yaml(config: dict, file_path: str):
         yaml.dump(config, f)
 
 
-def get_gguf_paths(directory: str) -> list[str]:
-    """Get paths of all files with '.gguf' extension in the specified directory and its subdirectories."""
+def get_extensions_paths(directory: str, extension: str) -> list[str]:
+    """Get paths of all files with '.{extension}' extension in the specified directory and its subdirectories."""
     gguf_paths = [
         os.path.join(root, name)
         for root, _, files in os.walk(directory)
         for name in files
-        if name.endswith(".gguf")
+        if name.endswith(f".{extension}")
     ]
     return gguf_paths
 
@@ -35,21 +35,10 @@ def get_filename(path: str) -> str:
     return os.path.basename(path)
 
 
-def get_xml_paths(directory: str) -> list[str]:
-    """Get paths of all files with '.xml' extension in the specified directory and its subdirectories."""
-    xml_paths = [
-        os.path.join(root, name)
-        for root, _, files in os.walk(directory)
-        for name in files
-        if name.endswith(".xml")
-    ]
-    return xml_paths
-
-
 def group_list(list, group_size):
     """Creates a list of lists of group_size"""
     k = len(list)
     grouped_list = []
     for k in range(0, k, group_size):
-        grouped_list.append(list[k : k + group_size])
+        grouped_list.append(list[k: k + group_size])
     return grouped_list
